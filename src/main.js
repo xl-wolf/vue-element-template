15ba8b12c06a1cca80899121f204a1eb1689df02
xl-wolf <1072236984@qq.com>
2020-05-29 11:21:45 +0800

    主题修改

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from "vue";
 import "normalize.css/normalize.css"; // 一个现代的css样式重置库
-import "@/styles/index.scss";
 import ElementUI from "element-ui";
-import "element-ui/lib/theme-chalk/index.css";
+// import "element-ui/lib/theme-chalk/index.css"; //在theme.scss中引入的@import "~element-ui/packages/theme-chalk/src/index";已经覆盖这边的样式，无需重复引入
+import "@/styles/index.scss";
 import "@/assets/xl-icon/iconfont.css"; //载入自定义图标字体
 
 import App from "./App.vue";
