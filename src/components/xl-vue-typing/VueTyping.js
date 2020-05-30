109d7daccb6e0a5cc3a0f4f52618ade457fc5776
xl-wolf <1072236984@qq.com>
2020-05-30 16:57:49 +0800

    引入文字逐个打印组件

---
+++ src/components/xl-vue-typing/VueTyping.js
@@ -0,0 +1,142 @@
+import { FrameIterator } from "./utils";
+
+export default {
+  render(h) {
+    if (this.disableCursor) {
+      return h("div", [h("span", this.speechFrameFragment)], {
+        style: {
+          color: "red",
+        },
+        on: {
+          click: this.$emit("textClick", this.speechFrameFragment),
+        },
+      });
+    } else
+      return h("div", [
+        h("span", this.speechFrameFragment),
+        h(
+          "span",
+          {
+            style: {
+              opacity: this.cursorOpacity,
+              color: this.cursorOptions.color,
+            },
+            class: { "typing-cursor": true },
+          },
+          this.cursorOptions.cursor
+        ),
+      ]);
+  },
+  props: {
+    framerate: {
+      type: Number,
+      default: 1,
+    },
+    text: {
+      type: String,
+      default: "",
+    },
+    rewrite: {
+      type: Boolean,
+      default: false,
+    },
+    cursorOptions: {
+      type: Object,
+      default: () => {
+        return {
+          blinking: true,
+          cursor: "|",
+          color: "black",
+          framerate: 24,
+        };
+      },
+    },
+    disableCursor: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  data() {
+    return {
+      speechFrameFragment: "",
+      frameIterator: new FrameIterator(),
+      cursorFrameIterator: new FrameIterator(),
+      cursorOpacity: 0,
+    };
+  },
+  watch: {
+    text() {
+      this.scheduleTyping();
+    },
+    framerate() {
+      this.scheduleTyping();
+    },
+    cursorOptions() {
+      this.blinkingCursor();
+    },
+  },
+  methods: {
+    async scheduleTyping() {
+      const { text } = this;
+      this.isHidden = false;
+      if (this.rewrite) {
+        await this.deleteText();
+      } else {
+        this.speechFrameFragment = "";
+      }
+
+      let idx = 0;
+      this.frameIterator
+        .schedule((breakLoop) => {
+          if (this.speechFrameFragment.length >= text.length) {
+            breakLoop();
+            return;
+          }
+          this.speechFrameFragment += text[idx];
+          idx++;
+        }, this.framerate)
+        .then(() => {
+          this.$emit("done");
+        });
+    },
+    deleteText() {
+      let idx = this.speechFrameFragment.length;
+      return this.frameIterator.schedule((breakLoop) => {
+        if (idx === -1) {
+          breakLoop();
+          return;
+        }
+        this.speechFrameFragment = this.speechFrameFragment.substr(0, idx);
+        idx--;
+      }, this.framerate);
+    },
+    blinkingCursor() {
+      console.log(this.cursorOptions);
+      const { framerate } = this.cursorOptions || this;
+      const { blinking } = this.cursorOptions;
+      let blinkingToggle = false;
+
+      if (!blinking) {
+        this.cursorOpacity = 1;
+        return;
+      }
+
+      this.cursorFrameIterator.schedule(() => {
+        blinkingToggle = !blinkingToggle;
+        this.cursorOpacity = blinkingToggle ? 1 : 0;
+      }, framerate);
+    },
+  },
+  created() {
+    this.speechFrameFragment = this.text;
+    if (!this.disableCursor) {
+      this.blinkingCursor();
+    }
+  },
+};
+
+export const cursorSpeed = {
+  slow: 40,
+  normal: 24,
+  fast: 5,
+};
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,12 +1,30 @@
 <template>
   <div class="app-main-container">
-    home
+    <el-card shadow="hover">
+      <vue-typing :text="typingText" :cursorOptions="myCursor" :framerate="4"/>
+    </el-card>
   </div>
 </template>
 
 <script>
+import VueTyping from '@/components/xl-vue-typing'
 export default {
-  name: 'home'
+  name: 'home',
+  components: { VueTyping },
+  data() {
+    return {
+      typingText: null,
+      myCursor: { blinking: true, cursor: '|', color: 'black', framerate: 24 }
+    }
+  },
+  mounted() {
+    this.startType()
+  },
+  methods: {
+    startType() {
+      this.typingText = `I'm home page,you can now begin your development in this frame,enjoy yourself a good journey!`
+    }
+  }
 }
 </script>
 
