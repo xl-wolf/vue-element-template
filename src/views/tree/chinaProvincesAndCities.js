bd7be0e8231dd54679bdd3ade42b2e33dd155cc1
xl-wolf <1072236984@qq.com>
2020-06-18 20:49:28 +0800

    data整理

--- src/views/tree/chinaProvincesAndCities.js
+++ src/views/tree/chinaProvincesAndCities.js
@@ -3,97 +3,97 @@ export default [
     nationName: "中国",
     provinces: [
       {
-        provinceName: "河北省",
+        provinceName: "河北",
       },
       {
-        provinceName: "山西省",
+        provinceName: "山西",
       },
       {
-        provinceName: "内蒙古自治区",
+        provinceName: "内蒙古",
       },
       {
-        provinceName: "辽宁省",
+        provinceName: "辽宁",
       },
       {
-        provinceName: "吉林省",
+        provinceName: "吉林",
       },
       {
-        provinceName: "黑龙江省",
+        provinceName: "黑龙江",
       },
       {
-        provinceName: "江苏省",
+        provinceName: "江苏",
       },
       {
-        provinceName: "浙江省",
+        provinceName: "浙江",
       },
       {
-        provinceName: "安徽省",
+        provinceName: "安徽",
       },
       {
-        provinceName: "福建省",
+        provinceName: "福建",
       },
       {
-        provinceName: "江西省",
+        provinceName: "江西",
       },
       {
-        provinceName: "山东省",
+        provinceName: "山东",
       },
       {
-        provinceName: "河南省",
+        provinceName: "河南",
       },
       {
-        provinceName: "湖北省",
+        provinceName: "湖北",
       },
       {
-        provinceName: "湖南省",
+        provinceName: "湖南",
       },
       {
-        provinceName: "广东省",
+        provinceName: "广东",
       },
       {
-        provinceName: "广西壮族自治区",
+        provinceName: "广西",
       },
       {
-        provinceName: "海南省",
+        provinceName: "海南",
       },
       {
-        provinceName: "四川省",
+        provinceName: "四川",
       },
       {
-        provinceName: "贵州省",
+        provinceName: "贵州",
       },
       {
-        provinceName: "云南省",
+        provinceName: "云南",
       },
       {
-        provinceName: "陕西省",
+        provinceName: "陕西",
       },
       {
-        provinceName: "甘肃省",
+        provinceName: "甘肃",
       },
       {
-        provinceName: "青海省",
+        provinceName: "青海",
       },
       {
-        provinceName: "西藏自治区",
+        provinceName: "西藏",
       },
       {
-        provinceName: "宁夏回族自治区",
+        provinceName: "宁夏",
       },
       {
         provinceName: "台湾",
       },
       {
-        provinceName: "北京市",
+        provinceName: "北京",
       },
       {
-        provinceName: "天津市",
+        provinceName: "天津",
       },
       {
-        provinceName: "上海市",
+        provinceName: "上海",
       },
       {
-        provinceName: "重庆市",
+        provinceName: "重庆",
       },
       {
         provinceName: "香港",
@@ -102,7 +102,7 @@ export default [
         provinceName: "澳门",
       },
       {
-        provinceName: "新疆维吾尔自治区",
+        provinceName: "新疆",
       },
     ],
   },
