bfc8816c0f4d28b7759aa626b2fdc68502479b26
xl-wolf <1072236984@qq.com>
2020-06-22 15:40:48 +0800

    local vuex learn

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 import Vue from "vue";
-import Vuex from "vuex";
+import Vuex from "./vuex";
 import app from "./modules/app";
 import settings from "./modules/settings";
 import user from "./modules/user";
