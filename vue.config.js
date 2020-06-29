09589d894ac87d51f4d4880b90c4f2949a5a9ff0
xl-wolf <1072236984@qq.com>
2020-06-29 09:36:36 +0800

    vue.config.js 配置热更新

--- vue.config.js
+++ vue.config.js
@@ -1,33 +1,35 @@
-const path = require('path')
-const defaultSettings = require('./src/settings.js')
+const path = require("path");
+const defaultSettings = require("./src/settings.js");
 
 function resolve(dir) {
-  return path.join(__dirname, dir)
+  return path.join(__dirname, dir);
 }
 
-const name = defaultSettings.title || 'vue-element-template' // page title
-const port = 8099 
+const name = defaultSettings.title || "vue-element-template"; // page title
+const port = 8099;
 module.exports = {
- 
-  publicPath: '/',
-  outputDir: 'dist',
-  assetsDir: 'static',
-  lintOnSave: false,//process.env.NODE_ENV === 'development', eslint校验与否
+  publicPath: "/",
+  outputDir: "dist",
+  assetsDir: "static",
+  lintOnSave: false, //process.env.NODE_ENV === 'development', eslint校验与否
   productionSourceMap: false,
   devServer: {
     port: port,
     open: true,
     overlay: {
       warnings: false,
-      errors: true
-    }
+      errors: true,
+    },
+  },
+  chainWebpack: (config) => {
+    config.resolve.symlinks(true); // 修复热更新失效
   },
   configureWebpack: {
     name: name,
     resolve: {
       alias: {
-        '@': resolve('src')
-      }
-    }
-  }
-}
+        "@": resolve("src"),
+      },
+    },
+  },
+};
