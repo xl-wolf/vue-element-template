a788c5192588e8c039fdaa47fb66adc6b8a0b549
xl-wolf <1072236984@qq.com>
2020-07-21 11:26:33 +0800

    vue.config.js生产构建的配置实践

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
 const path = require("path");
 const defaultSettings = require("./src/settings.js");
-const PurifyCSSPlugin=require('purifycss-webpack'); //css优化去重复无效代码
-const glob=require('glob');  //css优化
+// const glob = require("glob-all");
+// const PurgecssPlugin = require("purgecss-webpack-plugin");
+const resolve = dir => path.join(__dirname, dir);
+// console.log('process.env.NODE_ENV', process.env.NODE_ENV)
+// const IS_PROD = ["production", "prod"].includes(process.env.NODE_ENV);
 
-function resolve(dir) {
-  return path.join(__dirname, dir);
-}
 
 const name = defaultSettings.title || "vue-element-template"; // page title
 const port = 8099;
@@ -23,21 +23,48 @@ module.exports = {
       errors: true,
     },
   },
+  
   chainWebpack: (config) => {
     config.resolve.symlinks(true); // 修复热更新失效
+    config.resolve.alias.set("@", resolve("src"))
   },
-  configureWebpack: {
-    name: name,
-    resolve: {
-      alias: {
-        "@": resolve("src"),
-      },
-    },
-    plugins: [
-      new PurifyCSSPlugin({
-        // Give paths to parse for rules. These should be absolute!
-        paths: glob.sync(path.join(__dirname, 'app/*.html')),
-      })
-    ]
-  },
+
+
+  configureWebpack: config => {
+    // console.log('config',config)
+    // const plugins = [];
+    // if (IS_PROD) {
+    //   plugins.push(
+    //     new PurgecssPlugin({
+    //       paths: glob.sync([resolve("./**/*.vue")]),
+    //       extractors: [
+    //         {
+    //           extractor: new class Extractor {
+    //             static extract(content) {
+    //               const validSection = content.replace(
+    //                 /<style([\s\S]*?)<\/style>+/gim,
+    //                 ""
+    //               );
+    //               return (
+    //                 validSection.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || []
+    //               );
+    //             }
+    //           },
+    //           extensions: ["html", "vue"]
+    //         }
+    //       ],
+    //       whitelist: ["html", "body"],
+    //       whitelistPatterns: [
+    //         /el-.*/,
+    //         /-(leave|enter|appear)(|-(to|from|active))$/,
+    //         /^(?!cursor-move).+-move$/,
+    //         /^router-link(|-exact)-active$/
+    //       ],
+    //       whitelistPatternsChildren: [/^token/, /^pre/, /^code/]
+    //     })
+    //   );
+    // }
+    // config.plugins = [...config.plugins, ...plugins]
+    config.name = name
+  }
 };
